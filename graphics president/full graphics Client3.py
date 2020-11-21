ba97d97d089c918994bc7c012ae2a6cd4b371802
Dragon445 <63338955+Dragon445@users.noreply.github.com>
2020-11-21 17:07:02 +0000

    Add files via upload

---
+++ graphics president/full graphics Client3.py
@@ -0,0 +1,434 @@
+import socket,pickle,time,os
+import random
+import pygame
+
+from _thread import *
+pygame.font.init()
+global btns,messageToShow,x,y,roundsizebuttons,roundSize,message,myHand,autoUpdate,lastCardPlayed
+lastCardPlayed = 0
+autoUpdate = True
+message = ""
+roundsizebuttons = []
+
+x = 0
+y = 100
+roundSize = 0
+messageToShow = ""
+btns = []
+width = 1070
+height = 700
+background = pygame.image.load("images/background.jpg")
+def getevents(win):
+    global message
+    while True:
+        
+        message = s.recv(2048).decode()
+def displayMessage(text,x,y):
+  font = pygame.font.SysFont("yugothicyugothicuisemiboldyugothicuibold", 40)
+  text = font.render(text, 1, (255,255,255))
+  pygame.draw.rect(win,(0,0,0),(x,y,100,100))
+  pygame.draw.rect(win, (255,0,0), (x, y, text.get_width(), text.get_height()))
+  
+  win.blit(text,(x,y))
+  
+class Button:
+    def __init__(self, text, x, y, color,value):
+        self.text = text
+        self.x = x
+        self.y = y
+        self.color = color
+        self.width = 150
+        self.height = 100
+        self.value = value
+    def draw(self, win):
+        
+        pygame.draw.rect(win, self.color, (self.x, self.y, self.width, self.height))
+        font = pygame.font.SysFont("yugothicyugothicuisemiboldyugothicuibold", 40)
+        text = font.render(self.text, 1, (255,255,255))
+        win.blit(text, (self.x + round(self.width/2) - round(text.get_width()/2), self.y + round(self.height/2) - round(text.get_height()/2)))
+
+    def click(self, pos):
+        x1 = pos[0]
+        y1 = pos[1]
+        if self.x <= x1 <= self.x + self.width and self.y <= y1 <= self.y + self.height:
+            return True
+        else:
+            return False
+class cardButton:
+    def __init__(self,x, y,value):
+        
+        self.x = x
+        self.y = y
+        
+        self.width = 69
+        self.height = 94
+        self.value = value
+        self.image = pygame.image.load(r'images/' + str(value) + '-clubs.png') 
+    def draw(self, win):
+        win.blit(self.image,(self.x,self.y))
+        #font = pygame.font.SysFont("comicsans", 40)
+        #text = font.render(self.text, 1, (255,255,255))
+        #win.blit(text, (self.x + round(self.width/2) - round(text.get_width()/2), self.y + round(self.height/2) - round(text.get_height()/2)))
+
+    def click(self, pos):
+        x1 = pos[0]
+        y1 = pos[1]
+        if self.x <= x1 <= self.x + self.width and self.y <= y1 <= self.y + self.height:
+            return True
+        else:
+            return False
+selectbuttons = [Button("Create a game", 450, 200, (255,0,0),"1"),Button("join a game", 450, 350, (255,0,0),"2")]
+def joinscreen(win):
+    onscreen = True
+    code = " "
+    while onscreen == True:
+        mode = ""
+        win.fill((0,0,0))
+        displayMessage("code?",0,0)
+        displayMessage(code,0,100)
+        pygame.display.update()
+        if code == "":
+            code = " "
+        pygame.display.update()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+            if event.type == pygame.KEYDOWN:
+               
+                if event.key != 13:
+                    
+                    gotchar = str(chr(event.key))
+                    code = code + gotchar
+                if event.key == pygame.K_BACKSPACE:
+                    code = code[:-2]
+                if event.key == 13:
+                    code = code.replace(" ","")
+                    return code
+                #if event.key == pygame.K_ENTER:
+                  #  return code
+def setnamescreen(win):
+    onscreen = True
+    name = ""
+    while onscreen == True:
+        mode = ""
+        win.fill((0,0,0))
+        displayMessage("what is your name?",0,0)
+        displayMessage(name,0,100)
+        pygame.display.update()
+        if name == "":
+            name = " "
+        pygame.display.update()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+            if event.type == pygame.KEYDOWN:
+                
+                if event.key != 13:
+                    
+                    gotchar = str(chr(event.key))
+                    name = name + gotchar
+                if event.key == pygame.K_BACKSPACE:
+                    name = name[:-2]
+                if event.key == 13:
+                    name = name.replace(" ","")
+                    return name
+                #if event.key == pygame.K_ENTER:
+                  #  return code
+
+def createscreen(win):
+    onscreen = True
+    code = " "
+    while onscreen == True:
+        mode = ""
+        win.fill((0,0,0))
+        displayMessage("how many players?",0,0)
+        displayMessage(code,0,100)
+        pygame.display.update()
+            
+        pygame.display.update()
+        if code == "":
+            code = " "
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+            if event.type == pygame.KEYDOWN:
+                
+                if event.key != 13:
+                    
+                    gotchar = str(chr(event.key))
+                    code = code + gotchar
+                if event.key == pygame.K_BACKSPACE:
+                    code = code[:-2]
+                if event.key == 13:
+                    code = code.replace(" ","")
+                    return code
+                #if event.key == pygame.K_ENTER:
+                  #  return code
+
+
+def mainscreen(win):
+    win.fill((0,0,0))
+    font = pygame.font.SysFont("yugothicyugothicuisemiboldyugothicuibold", 120)
+    text = font.render("Online President", 1, (100,100,200))
+    win.blit(text, (40,0))
+
+    onscreen = True
+    while onscreen == True:
+        mode = ""
+        
+        for btn in selectbuttons:
+            btn.draw(win)
+            
+        pygame.display.update()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                pos = pygame.mouse.get_pos()
+                for btn in selectbuttons:
+                    if btn.click(pos):
+
+                        mode = btn.value
+                        if mode == "1":
+                            return 1
+                        if mode == "2":
+                            return 2
+           
+s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+#name = input("what is your name: ")
+#mode = int(input("what do you want to do? to Join a room: type 1, to start a room,type 2"))
+
+lastCardPlayed = 0
+myHand = []
+highestset = 1
+win = pygame.display.set_mode((width, height))
+pygame.display.set_caption("Client")
+name = setnamescreen(win)
+joinmode = mainscreen(win)
+if joinmode == 1:
+    size = createscreen(win)
+    code = "startRoom"
+    
+    
+    
+    s.connect(("192.168.0.14",5555))
+    s.send(str.encode(code + ":" + name + ":" + size))
+if joinmode == 2:
+    code = joinscreen(win)
+    s.connect(("192.168.0.14",5555))
+    
+    s.send(str.encode(code + ":" + name))
+start_new_thread(getevents,(win, ))
+def countOccurrences(card,hand):
+    count = 0
+    for i in range(len(hand)):
+        if hand[i] == card:
+            count += 1
+    return count
+def playTurn(temp):
+    global roundSize,lastCardPlayed,messageToShow,x,y,btns
+    btns = []
+    x = 0
+    y = 100
+    win.fill((0,0,0))
+    hand = temp
+    highestset = 0
+    cardplaynumber = 0
+    
+    setlist = []
+    for i in range(15):
+        tempsetlistelement = countOccurrences(i,myHand)
+        setlist.append(tempsetlistelement)
+        setlist.sort()
+        highestset = setlist[-1]
+    displayMessage("your Turn",0,0)
+    displayMessage(messageToShow,0,300)
+    displayMessage("round size: " + str(roundSize),800,0)
+    for i in range(len(hand)):
+        btns.append(cardButton(x,y,hand[i]))
+        
+        x+=69
+        try:
+            if hand[i] != hand[i + 1]:
+                x += 20
+        except:
+            pass
+        if x > 1000:
+            y += 94
+            x = 0
+    x = 0
+    y = 100
+    btns.append(Button("out", 250, 500, (255,0,0),"out"))
+    for buttons in btns:
+                buttons.draw(win)
+    
+    pygame.display.update()
+    canplaycard = False
+    card = 0
+    while canplaycard == False:
+        
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                pos = pygame.mouse.get_pos()
+                for btn in btns:
+                    if btn.click(pos):
+                        card = btn.value
+        if card != "" and card != 0:
+            if card == "out":
+                return "out",lastCardPlayed
+            if hand[-1] <= lastCardPlayed:
+                return "out",lastCardPlayed
+            if highestset < roundSize:
+                return "out",lastCardPlayed
+            
+            if card != "out":
+                card = int(card)
+                cardplaynumber = countOccurrences(card,hand)
+            if lastCardPlayed < card and cardplaynumber >= roundSize and card in hand:
+                
+                
+                for i in range(roundSize):
+                  hand.remove(card)
+               
+                canplaycard = True
+                break
+            if lastCardPlayed >= card:
+                displayMessage("card too small",0,300)
+                card = 0
+    btns = []
+    x = 0
+    y = 100
+    win.fill((0,0,0))
+    for i in range(len(hand)):
+        btns.append(cardButton(x,y,hand[i]))
+        
+        x+=69
+        try:
+            if hand[i] != hand[i + 1]:
+                x += 20
+        except:
+            pass
+        if x > 1000:
+            y += 94
+            x = 0
+    x = 0
+    y = 100
+    for btn in btns:
+        btn.draw(win)
+    pygame.display.update()
+    return hand,card
+isNotOut = True
+
+while True:
+    win.fill((0,0,0))
+    for buttons in btns:
+        buttons.draw(win)
+    displayMessage(messageToShow,0,300)
+    displayMessage("round size: " + str(roundSize),800,0)
+    pygame.display.update()
+    
+    for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+    if message != "":
+        
+        instruction1 = message.split(":")
+        message = ""
+        instruction = instruction1[0]
+        contents = instruction1[1]
+        if instruction == "updateHand":
+            myHand = []
+            contents = contents.replace("[","")
+            contents = contents.replace("]","")
+            contents = contents.replace(" ","")
+            contents = contents.split(",")
+            for i in range(len(contents)):
+                myHand.append(int(contents[i]))
+            
+            hand = myHand
+            for i in range(len(hand)):
+                btns.append(cardButton(x,y,hand[i]))
+                
+                x+=69
+                try:
+                    if hand[i] != hand[i + 1]:
+                        x += 20
+                except:
+                    pass
+                if x > 1000:
+                    y += 94
+                    x = 0
+            x = 0
+            y = 100
+        if instruction == "setRoundSize":
+            
+            highestset = 0
+            cardplaynumber = 0
+            
+            setlist = []
+            for i in range(15):
+                tempsetlistelement = countOccurrences(i,myHand)
+                setlist.append(tempsetlistelement)
+                setlist.sort()
+                highestset = setlist[-1]
+            
+            
+            cansetsize = False
+            displayMessage("how many cards do u want to play? ",0,350)
+            while cansetsize == False:
+                
+                 for i in range(highestset):
+                    roundsizebuttons.append(Button(str(i + 1), x, y + 300, (255,0,0),i + 1))
+                    x += 200
+                 x = 0
+                 for button in roundsizebuttons:
+                     button.draw(win)
+                 pygame.display.update()
+                 temproundsize = 0
+                 for event in pygame.event.get():
+                    if event.type == pygame.QUIT:
+                        pygame.quit()
+                        quit()
+                    if event.type == pygame.MOUSEBUTTONDOWN:
+                        pos = pygame.mouse.get_pos()
+                        for btn in roundsizebuttons:
+                            if btn.click(pos):
+                                temproundsize = btn.value
+                 if temproundsize <= highestset and temproundsize != 0:
+                    roundSize = temproundsize
+                    s.send(str.encode(str(roundSize)))
+                    cansetsize = True
+                    temproundsizebuttons = []
+                    message = ""
+                    pygame.display.update()
+                   
+        if instruction == "playTurn":
+            
+            contents = int(contents)
+            roundSize = int(instruction1[2])
+            lastCardPlayed = contents            
+            playedmove, playedcard = playTurn(myHand)
+            if playedmove != "out":
+                myHand = playedmove
+            s.send(str.encode("playedTurn:0"))
+            time.sleep(1)
+            s.send(pickle.dumps(playedmove))
+            time.sleep(1)
+            s.send(pickle.dumps(playedcard))
+        if instruction == "displayMessage":
+            for buttons in btns:
+                    buttons.draw(win)
+            messageToShow = contents
+            
+            
+            
+        if instruction == "goneOut":
+            print("you have are " + "# " + contents)
---
+++ graphics president/graphics President Server1.py
@@ -0,0 +1,258 @@
+#code by Ethan Brammah for an online card game of president.uses only baisic libraries. i cant spell btw
+from _thread import*
+import socket
+import random
+import time
+import string
+s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+def id_generator(size=6, chars=string.digits):#room id genorator
+            return ''.join(random.choice(chars) for x in range(size))
+s.bind(("192.168.0.14",5555))#creates server
+s.listen(100)
+import pickle
+global rooms#room storage veriable, rooms are stored as classes under keys of their ids
+# game function
+def playGame(roomid):
+        
+    while rooms[roomid].playable == False:
+       try:
+            
+                
+            if rooms[roomid].players == 1:
+                broadcast("displayMessage:game over.",rooms[roomid].roomid)
+                break
+            if rooms[roomid].turn in rooms[roomid].canPlay:
+                
+            
+                if rooms[roomid].playable == False:
+                    playedcard = 0
+                    playedmove = []
+                    if rooms[roomid].starter == rooms[roomid].turn:
+                        rooms[roomid].canPlayConn[rooms[roomid].turn].send(str.encode("setRoundSize:0"))
+                        rooms[roomid].roundSize = rooms[roomid].canPlayConn[rooms[roomid].turn].recv(2048).decode()
+                        rooms[roomid].starter = -1
+                        broadcast("displayMessage:"+ rooms[roomid].playerNames[rooms[roomid].turn] + " has set the round size to " + str(rooms[roomid].roundSize),rooms[roomid].roomid)
+                        
+                    if len(rooms[roomid].playersHands[rooms[roomid].turn]) != 0:         
+                        rooms[roomid].canPlayConn[rooms[roomid].turn].send(str.encode("playTurn:" + str(rooms[roomid].lastCardPlayed) + ":" + str(rooms[roomid].roundSize)))
+                        try:
+                            move = rooms[roomid].canPlayConn[rooms[roomid].turn].recv(2048).decode()
+                        except:
+                            broadcast("displayMessage:an error has occoured, please play your turn again.",roomid)
+                            rooms[roomid].canPlayConn[rooms[roomid].turn].send(str.encode("updateHand:"+str(rooms[roomid].playersHands[rooms[roomid].turn])))
+                            
+                            continue
+                        splitmove = move.split(":")
+                        if splitmove[0] == "playedTurn":
+                            playedmove = pickle.loads(rooms[roomid].canPlayConn[rooms[roomid].turn].recv(2048))
+                            
+                            playedcard = pickle.loads(rooms[roomid].canPlayConn[rooms[roomid].turn].recv(2048))
+                            if playedmove == "out":
+                                rooms[roomid].canPlay.remove(rooms[roomid].turn)
+                                
+                                broadcast("displayMessage:" + rooms[roomid].playerNames[rooms[roomid].turn] +" is out",rooms[roomid].roomid)
+                            
+                            rooms[roomid].playersHands[rooms[roomid].turn] = playedmove
+                            rooms[roomid].lastCardPlayed = playedcard
+                            if len(playedmove) == 0:
+                                rooms[roomid].players -= 1
+                                rooms[roomid].canPlay = []
+                                rooms[roomid].canPlayConn[rooms[roomid].turn].send(str.encode("goneOut:" + str(rooms[roomid].winners)))
+                                
+                                
+                                broadcast("displayMessage:" + rooms[roomid].playerNames[rooms[roomid].turn] + " has got rid of all their cards. they are #" + str(rooms[roomid].winners) + ". they have " + str(len(playedmove)) + " cards left",rooms[roomid].roomid)
+                                rooms[roomid].goneOut.append(rooms[roomid].turn)
+                                del rooms[roomid].canPlayConn[rooms[roomid].turn]
+                                del rooms[roomid].playerNames[rooms[roomid].turn]
+                                del rooms[roomid].playersHands[rooms[roomid].turn]#stops this player from playing again. inelgennt and horrible but it works
+                               
+                                rooms[roomid].winners += 1
+                                rooms[roomid].lastCardPlayed = 0
+                                rooms[roomid].roundSize = 0
+                                rooms[roomid].starter = rooms[roomid].turn
+                                rooms[roomid].winners += 1
+                                for i in range(rooms[roomid].players):
+                                    
+                                    rooms[roomid].canPlay.append(i)
+                                if rooms[roomid].turn == rooms[roomid]. players:
+                                    turn = 0
+                                continue
+                                
+                            if playedmove != "out":
+                                broadcast("displayMessage:" + rooms[roomid].playerNames[rooms[roomid].turn] + " has played a " + str(playedcard) + ". they have " + str(len(playedmove)) + " cards left",rooms[roomid].roomid)
+                
+                            if rooms[roomid].lastCardPlayed != 14 and len(rooms[roomid].canPlay) != 1 or len(playedmove) == 0:
+                                rooms[roomid].turn += 1
+                            if rooms[roomid].lastCardPlayed == 14:
+                                 rooms[roomid].canPlay = []
+                                 rooms[roomid].starter = rooms[roomid].turn
+                                 for i in range(rooms[roomid].players):
+                                    
+                                    rooms[roomid].canPlay.append(i)
+                                 broadcast("displayMessage:" + rooms[roomid].playerNames[rooms[roomid].turn] +" won this round. they now start",rooms[roomid].roomid)
+                                 
+                                 rooms[roomid].lastCardPlayed = 0
+                            if len(rooms[roomid].canPlay) == 1:
+                                rooms[roomid].turn = rooms[roomid].canPlay[-1]
+                                broadcast("displayMessage:" + rooms[roomid].playerNames[rooms[roomid].turn] +" won this round. they now start",rooms[roomid].roomid)
+                                rooms[roomid].starter = rooms[roomid].turn
+                                rooms[roomid].canPlay = []
+                                rooms[roomid].lastCardPlayed = 0
+                                for i in range(rooms[roomid].players):
+                                    
+                                    rooms[roomid].canPlay.append(i)
+                            if rooms[roomid].turn == rooms[roomid].players:
+                                rooms[roomid].turn = 0
+            else:
+                if rooms[roomid].turn != len(rooms[roomid].canPlay):
+                    rooms[roomid].turn += 1#if player has gone out make it the next players turn
+                if rooms[roomid].turn == len(rooms[roomid].canPlay):
+                    rooms[roomid].turn = 0
+       except socket.error as e:
+            print(e)
+            del rooms[roomid].connList[rooms[roomid].turn]
+            broadcast("displayMessage:someone has left this game or an error has occured. this room is being terminated",roomid)
+            del rooms[roomid]#if there is an error or someone leaves this room is deleted
+            break
+def broadcast(message,roomid):
+    #send a message to all clients within a secific room
+    try:
+        for o in range(len(rooms.get(roomid).connList)):
+            rooms.get(roomid).connList[o].send(str.encode(message))
+    except:
+        pass
+    time.sleep(0.2)
+def playTurn(temp,lastcard):#unused function, this is used in the client to play turns
+            lastCardPlayed = lastcard
+            hand = temp
+            if lastCardPlayed > hand[-1]:
+                return "out",lastCardPlayed
+            card = 0
+            canplaycard = False
+            while canplaycard == False:
+                
+                card = input("which card would you like to play? your hand is " + str(hand))
+                if card == "out":
+                    return "out",lastCardPlayed
+                if card != "out":
+                    card = int(card)
+                if lastCardPlayed < card:
+                    
+                    
+                    for i in range(len(hand)):
+                        if hand[i] == card:
+                            del hand[i]
+                            
+                            canplaycard = True
+                            break
+                if lastCardPlayed >= card:
+                    print("card too small")
+            return hand,card
+class Client:#room client, stores all the information required to play, along with the dealing algorythms and a function to join this room
+    def __init__(self,players,roomid):
+        self.lastCardPlayed = 0
+        self.roomid = roomid
+        self.playerNames = []
+        self.playersHands,self.playersList,self.canPlay = self.shuffleDeal(players)
+        self.connList = []
+        self.roundSize = 1
+        self.playable = False
+        self.tempPlayerHands = {}
+        self.canPlayConn = []
+        self.turn = 0
+        self.starter = 0
+        self.winners = 1
+        self.players = players
+        self.onlineList = []
+        self.goneOut = []
+    def shuffleDeal(self,players):
+        self.lastCardPlayed = 1
+       
+        temphand = []
+        
+        playersHands = []
+        playersHands = []
+        playersList = []
+        canPlay = []
+        cards = [
+                2,2,2,2,
+                3,3,3,3,
+                4,4,4,4,
+                5,5,5,5,
+                6,6,6,6,
+                7,7,7,7,
+                8,8,8,8,
+                9,9,9,9,
+                10,10,10,10,
+                11,11,11,11,
+                12,12,12,12,
+                13,13,13,13,
+                14,14,14,14]
+        
+        cards2 = cards
+
+        for i in range(players):
+            playersHands.append([])
+            canPlay.append(i)
+
+        person = 0
+        for i in range(len(cards2)):
+
+            if person == players:
+              person = 0
+
+            if len(cards2) == 0:
+              break
+
+
+            card = random.choice(cards2)
+            cards2.remove(card)
+
+            playersHands[person].append(card)
+           
+            person += 1
+
+        for o in range(players):
+            
+            temphand = playersHands[o]
+            temphand.sort()
+            
+            
+        return playersHands,playersList,canPlay
+    def joinRoom(self,conn,name):
+        time.sleep(1)
+        conn.send(str.encode("updateHand:" + str(self.playersHands[len(self.connList)])))
+        self.canPlayConn.append(conn)
+        self.connList.append(conn)
+        self.playerNames.append(name)
+        time.sleep(0.5)
+rooms = {"123456":Client(2,"123456"),"654321":Client(2,"654321")}#couple of pre-defined rooms for testing
+
+
+while True:
+    conn,addr = s.accept()
+    instruction = conn.recv(2048).decode()
+    instruction = instruction.split(":")
+    
+
+    if instruction[0] == "startRoom":#if client requests to create a room, create a custom code and create a room instance in the rooms dictionary under that key
+        newroomid = id_generator()
+        size = int(instruction[2])
+        rooms[newroomid] = Client(size,newroomid)
+        conn.send(str.encode("displayMessage:your room code is " + str(newroomid)))
+        time.sleep(0.1)
+        rooms[newroomid].joinRoom(conn,instruction[1])
+        rooms[newroomid].onlineList.append(instruction[1])
+        continue
+    if instruction[0] in rooms:
+        rooms[instruction[0]].joinRoom(conn,instruction[1])#join a room with the id provided
+        
+        rooms[instruction[0]].onlineList.append(instruction[1])
+        broadcast("displayMessage: " + instruction[1] + " joined the game",instruction[0])#alert everyone in that room whos joined
+        broadcast("displayMessage:online players; " + str(rooms[instruction[0]].onlineList),instruction[0])
+        if len(rooms[instruction[0]].connList) == rooms[instruction[0]].players:
+            start_new_thread(playGame, (instruction[0], ))
+            
+    if instruction[0] not in rooms and instruction[0] != "startRoom":
+        conn.send(str.encode("displayMessage:game not found, reload client and try again"))
+        conn.close()
